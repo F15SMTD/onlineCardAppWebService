98eb96526b588f4e52ebbaa3b5194659d85cd67c
Hamizan <23025494@myrp.edu.sg>
2026-01-05 12:22:42 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//include the require packages
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    WaitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0
+};  
+
+//initialize express app
+const app = express();
+//helps app to read json
+app.use(express.json());
+
+//start the server
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
+
+//example route: get all cards
+app.get('/cards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allcards' });
+    }
+});
+
