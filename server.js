3cbe3a64b0a50dada473f8d403739ff8634616fa
Hamizan <23025494@myrp.edu.sg>
2026-01-06 12:04:36 +0800

    added the car table and its stuff

--- server.js
+++ server.js
@@ -26,6 +26,31 @@ app.listen(port, () => {
     console.log('Server is running on port', port);
 });
 
+//example route: get all cars
+app.get('/allcars', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allcards' });
+    }
+});
+
+//example route: create a new card
+app.post('/addcar', async (req, res) => {
+    const { car_name, car_price, car_image } = req.body;
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO cards (car_name, car_price, car_image) VALUES (?, ?, ?)',[car_name, car_price, car_image]);
+        res.status(201).json({ message: 'Card' +car_name + 'successfully added' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error - could not add card '+ car_name});
+    }
+}); 
+
 //example route: get all cards
 app.get('/allcards', async (req, res) => {
     try {
